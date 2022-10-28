dd313a682b09918e1fc58588f4854deb14f562d4
indunil-inova <116708579+indunil-inova@users.noreply.github.com>
2022-10-28 05:54:26 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+# --- vpc.tf ---
+
+resource "aws_vpc" "prod-vpc" {
+    cidr_block              = "10.0.0.0/16"
+    enable_dns_support      = "true" 
+    enable_dns_hostnames    = "true" 
+    instance_tenancy        = "default"    
+    
+    tags = {
+        Name = "prod-vpc"
+    }
+}
